e1a1dd37bb3fde0724dee941ce18a84b770a0807
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-05-22 11:04:36 +0200

    uncomment Aqua tests again

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,16 +1,16 @@
-# @testitem "Aqua.jl" begin
-#     import Aqua
-#     using ExplicitImports: check_no_implicit_imports, check_no_stale_explicit_imports,
-#                            check_all_explicit_imports_via_owners,
-#                            check_all_qualified_accesses_via_owners,
-#                            check_no_self_qualified_accesses
+@testitem "Aqua.jl" begin
+    import Aqua
+    using ExplicitImports: check_no_implicit_imports, check_no_stale_explicit_imports,
+                           check_all_explicit_imports_via_owners,
+                           check_all_qualified_accesses_via_owners,
+                           check_no_self_qualified_accesses
 
-#     Aqua.test_all(SimpleDiscontinuousGalerkin)
-#     @test isnothing(check_no_implicit_imports(SimpleDiscontinuousGalerkin,
-#                                               skip = (Core, Base,
-#                                                       SimpleDiscontinuousGalerkin.SummationByPartsOperators)))
-#     @test isnothing(check_no_stale_explicit_imports(SimpleDiscontinuousGalerkin))
-#     @test isnothing(check_all_qualified_accesses_via_owners(SimpleDiscontinuousGalerkin;
-#                                                             ignore = (:ustrip,)))
-#     @test isnothing(check_no_self_qualified_accesses(SimpleDiscontinuousGalerkin))
-# end
+    Aqua.test_all(SimpleDiscontinuousGalerkin)
+    @test isnothing(check_no_implicit_imports(SimpleDiscontinuousGalerkin,
+                                              skip = (Core, Base,
+                                                      SimpleDiscontinuousGalerkin.SummationByPartsOperators)))
+    @test isnothing(check_no_stale_explicit_imports(SimpleDiscontinuousGalerkin))
+    @test isnothing(check_all_qualified_accesses_via_owners(SimpleDiscontinuousGalerkin;
+                                                            ignore = (:ustrip,)))
+    @test isnothing(check_no_self_qualified_accesses(SimpleDiscontinuousGalerkin))
+end
