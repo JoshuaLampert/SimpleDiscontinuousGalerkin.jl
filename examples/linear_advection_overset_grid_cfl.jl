3accdf6c327f617e9e59f2f3f8eae3661dc9aa33
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2025-07-29 11:45:28 +0200

    Fix `StepsizeCallback` for overset grid method (#24)

---
+++ examples/linear_advection_overset_grid_cfl.jl
@@ -0,0 +1,42 @@
+using SimpleDiscontinuousGalerkin
+using OrdinaryDiffEqLowStorageRK
+
+###############################################################################
+# semidiscretization of the linear advection equation
+
+advection_velocity = 2.0
+equations = LinearAdvectionEquation1D(advection_velocity)
+
+initial_condition = initial_condition_convergence_test
+
+# Create DG solver with polynomial degree = 3 and Godunov flux as surface flux
+solver = DGSEM(polydeg = 3, surface_flux = flux_godunov)
+
+a = -1.0
+b = -0.1
+c = 0.1
+d = 1.0
+
+N_elements = 10 # number of elements
+mesh_left = Mesh(a, c, N_elements)
+mesh_right = Mesh(b, d, N_elements)
+mesh = OversetGridMesh(mesh_left, mesh_right)
+
+# A semidiscretization collects data structures and functions for the spatial discretization
+semi = Semidiscretization(mesh, equations, initial_condition, solver)
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+# Create ODE problem with time span from 0.0 to 1.0
+tspan = (0.0, 1.0)
+ode = semidiscretize(semi, tspan)
+summary_callback = SummaryCallback()
+analysis_callback = AnalysisCallback(semi; interval = 10,
+                                     extra_analysis_errors = (:conservation_error,))
+stepsize_callback = StepsizeCallback(cfl = 0.5)
+callbacks = CallbackSet(analysis_callback, summary_callback, stepsize_callback)
+
+saveat = range(tspan..., length = 100)
+sol = solve(ode, RDPK3SpFSAL49(), adaptive = false, dt = 1.0,
+            save_everystep = false, callback = callbacks, saveat = saveat)
