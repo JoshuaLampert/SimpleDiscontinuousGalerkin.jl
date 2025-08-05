268075197ec9a4d4e44d8496f6174cfd476a25d4
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-08-05 14:36:33 +0200

    add unit tests

--- test/test_unit.jl
+++ test/test_unit.jl
@@ -62,6 +62,13 @@ end
     @test flux_godunov.(u_2, u_2, equations_maxwell) == flux.(u_2, equations_maxwell)
     @test flux_lax_friedrichs.(u_2, u_2, equations_maxwell) == flux.(u_2, equations_maxwell)
 
+    u_3 = [SVector(1.0, 2.0, -2.0), SVector(-2.0, -3.0, 3.0)]
+    equations_compressible_euler = @test_nowarn CompressibleEulerEquations1D(1.4)
+    @test flux_lax_friedrichs.(u_3, u_3, equations_compressible_euler) ==
+          flux.(u_3, equations_compressible_euler)
+    @test flux_ranocha.(u_3, u_3, equations_compressible_euler) ==
+          flux.(u_3, equations_compressible_euler)
+
     @test_nowarn print(FluxLaxFriedrichs())
     @test_nowarn display(FluxLaxFriedrichs())
     @test_nowarn print(SimpleDiscontinuousGalerkin.DissipationLocalLaxFriedrichs())
