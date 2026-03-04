99f36a6a75128cb155c8f7ed7199ff1762c6ad36
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-03-04 16:59:21 +0100

    ref value Maxwell

--- test/test_maxwell.jl
+++ test/test_maxwell.jl
@@ -1,7 +1,7 @@
 @testitem "maxwell_basic.jl" setup=[Setup] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR_MAXWELL, "maxwell_basic.jl"),
-                        l2=[0.0014845551649291684, 0.0018613613260282855],
-                        linf=[0.003311775580174814, 0.0029746676945732464],
+                        l2=[0.0010497390241669513, 0.0013161812158726952],
+                        linf=[0.00331177558017548, 0.002974667694574537],
                         cons_error=[2.498001805406602e-16, 5.342948306008566e-16],
                         change_entropy=-0.000112697123739125,
                         entropy_timederivative=-0.00011456197705196625)
