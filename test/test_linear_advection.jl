478ad7c6049c923eeb5000d3818648f27cbf06b2
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-03-04 17:07:08 +0100

    ref values linear advection

--- test/test_linear_advection.jl
+++ test/test_linear_advection.jl
@@ -1,6 +1,6 @@
 @testitem "linear_advection.jl" setup=[Setup] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION, "linear_advection.jl"),
-                        l2=[0.0001574641423858981], linf=[0.00042895011704513486],
+                        l2=[0.0001113439628747987], linf=[0.0004289501170462451],
                         cons_error=[4.3021142204224816e-16],
                         change_mass=-3.0531133177191805e-16,
                         change_entropy=-9.082223088596741e-7,
@@ -10,7 +10,7 @@
     @testset "Different surface flux on boundary and interior" begin
         @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION, "linear_advection.jl"),
                             surface_flux=surface_flux,
-                            l2=[0.0004272537469843011], linf=[0.0008162855376093736],
+                            l2=[0.00030211402178005256], linf=[0.0008162855376091516],
                             cons_error=[7.7021722333370235e-16],
                             change_mass=7.7021722333370235e-16,
                             change_entropy=-4.29859495665319e-7,
@@ -21,7 +21,7 @@
     @testset "flux_lax_friedrichs as surface flux" begin
         @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION, "linear_advection.jl"),
                             surface_flux=surface_flux,
-                            l2=[0.00015746414238601946], linf=[0.00042895011704702224],
+                            l2=[0.00011134396287476726], linf=[0.00042895011704513486],
                             cons_error=[3.191891195797325e-16],
                             change_mass=3.191891195797325e-16,
                             change_entropy=-9.082223089151853e-7,
@@ -32,7 +32,7 @@ end
 @testitem "linear_advection_Dirichlet_boundary_condition.jl" setup=[Setup] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                  "linear_advection_Dirichlet_boundary_condition.jl"),
-                        l2=[0.00019584095949362165], linf=[0.0006856564226139367],
+                        l2=[0.00013848047049198924], linf=[0.0006856564226135481],
                         change_mass=-0.5462956322467551,
                         change_entropy=-0.1978580355978945,
                         entropy_timederivative=-0.0066261099903804676)
@@ -41,7 +41,7 @@ end
 @testitem "linear_advection_strong_form.jl" setup=[Setup] begin
     # Same errors as in "linear_advection.jl"
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION, "linear_advection_strong_form.jl"),
-                        l2=[0.00015746414238620223], linf=[0.0004289501170468002],
+                        l2=[0.00011134396287493107], linf=[0.00042895011704668917],
                         change_mass=2.7755575615628914e-17,
                         change_entropy=-9.082223090817187e-7,
                         entropy_timederivative=-9.210638493017953e-7)
@@ -50,7 +50,7 @@ end
 @testitem "linear_advection_FDSBP.jl" setup=[Setup] begin
     # Same errors as in "linear_advection.jl"
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION, "linear_advection_strong_form.jl"),
-                        l2=[0.00015746414238620223], linf=[0.0004289501170468002],
+                        l2=[0.00011134396287493107], linf=[0.00042895011704668917],
                         change_mass=2.7755575615628914e-17,
                         change_entropy=-9.082223090817187e-7,
                         entropy_timederivative=-9.210638493017953e-7)
@@ -60,7 +60,7 @@ end
     # Same errors as in "linear_advection.jl" with `surface_flux = (flux_central, flux_godunov)`
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION, "linear_advection_FDSBP_SAT.jl"),
                         boundary_conditions=boundary_condition_periodic,
-                        l2=[0.00042725374698477996], linf=[0.0008162855376108169],
+                        l2=[0.00030211402178029147], linf=[0.0008162855376137035],
                         change_mass=-3.469446951953615e-16,
                         change_entropy=-4.298594954432744e-7,
                         entropy_timederivative=-6.420623096525803e-7)
@@ -70,7 +70,7 @@ end
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION, "linear_advection_FDSBP_SAT.jl"),
                         boundary_conditions=boundary_condition_periodic,
                         D=D,
-                        l2=[0.000157464142385992], linf=[0.0004289501170479104],
+                        l2=[0.00011134396287483056], linf=[0.0004289501170487986],
                         change_mass=-2.7755575615628914e-17,
                         change_entropy=-9.082223083600738e-7,
                         entropy_timederivative=-9.21063849912418e-7)
@@ -78,7 +78,7 @@ end
 
 @testitem "linear_advection_FD.jl" setup=[Setup] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION, "linear_advection_FD.jl"),
-                        l2=[0.03199259748601379], linf=[0.031230470786942688],
+                        l2=[0.015996298743007602], linf=[0.031230470786942688],
                         change_mass=0.00022039403436682758,
                         change_entropy=0.0038255363796113606,
                         entropy_timederivative=-0.0011976377220896395)
@@ -96,7 +96,7 @@ end
         # Same errors as in "linear_advection.jl"
         @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION, "linear_advection_FD.jl"),
                             semi=semi,
-                            l2=[0.00015746414238620223], linf=[0.0004289501170468002],
+                            l2=[0.00011134396287493107], linf=[0.00042895011704668917],
                             change_mass=2.7755575615628914e-17,
                             change_entropy=-9.082223090817187e-7,
                             entropy_timederivative=-9.210638493017953e-7)
@@ -107,7 +107,7 @@ end
     # Same errors as in "linear_advection.jl"
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                  "linear_advection_flux_differencing.jl"),
-                        l2=[0.00015746414238611707], linf=[0.0004289501170472443],
+                        l2=[0.00011134396287489659], linf=[0.0004289501170469112],
                         change_mass=4.2327252813834093e-16,
                         change_entropy=-9.08222309914386e-7,
                         entropy_timederivative=-9.21063850300996e-7)
@@ -117,7 +117,7 @@ end
     # Same errors as in "linear_advection.jl"
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                  "linear_advection_flux_differencing_strong_form.jl"),
-                        l2=[0.00015746414238603103], linf=[0.00042895011704957575],
+                        l2=[0.00011134396287494402], linf=[0.0004289501170479104],
                         change_mass=5.412337245047638e-16,
                         change_entropy=-9.082223089151853e-7,
                         entropy_timederivative=-9.210638502454849e-7)
@@ -126,7 +126,7 @@ end
 @testitem "linear_advection_cfl.jl" setup=[Setup] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                  "linear_advection_cfl.jl"),
-                        l2=[0.00029528077087911356], linf=[0.0006703908593475028],
+                        l2=[0.00020879503544234674], linf=[0.0006703908593470587],
                         change_mass=-4.163336342344337e-17,
                         change_entropy=-0.0002495788935772403,
                         entropy_timederivative=-9.203964659865171e-7)
@@ -136,7 +136,7 @@ end
         @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                      "linear_advection_cfl.jl"),
                             cfl=t -> 1.0,
-                            l2=[0.00029528077087911356], linf=[0.0006703908593475028],
+                            l2=[0.00020879503544234674], linf=[0.0006703908593470587],
                             change_mass=-4.163336342344337e-17,
                             change_entropy=-0.0002495788935772403,
                             entropy_timederivative=-9.203964659865171e-7)
@@ -151,7 +151,7 @@ end
 @testitem "linear_advection_inhomogeneous_mesh.jl" setup=[Setup] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                  "linear_advection_inhomogeneous_mesh.jl"),
-                        l2=[0.00015505641092054123], linf=[0.0004295567744543316],
+                        l2=[0.00010964143962832084], linf=[0.0004295567744558859],
                         cons_error=[7.632783294297951e-17],
                         change_mass=7.632783294297951e-17,
                         change_entropy=-8.259112555530912e-7,
@@ -164,7 +164,7 @@ end
         @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                      "linear_advection_inhomogeneous_mesh.jl"),
                             mesh=mesh,
-                            l2=[0.0001574641423857773], linf=[0.00042895011704657815],
+                            l2=[0.00011134396287483257], linf=[0.00042895011704469077],
                             cons_error=[3.95516952522712e-16],
                             change_mass=3.95516952522712e-16,
                             change_entropy=-9.082223094147857e-7,
@@ -174,7 +174,7 @@ end
 
 @testitem "linear_advection_per_element.jl" setup=[Setup] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION, "linear_advection_per_element.jl"),
-                        l2=[0.0033676357851732484], linf=[0.009084487338805292],
+                        l2=[0.0023812781002631356], linf=[0.009084487338802849],
                         cons_error=[7.494005416219807e-16],
                         change_mass=-7.494005416219807e-16,
                         change_entropy=-0.00025256281830143834,
@@ -188,7 +188,7 @@ end
         @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                      "linear_advection_per_element.jl"),
                             solver=solver,
-                            l2=[0.0033676357851732484], linf=[0.009084487338805292],
+                            l2=[0.0023812781002628585], linf=[0.009084487338804959],
                             cons_error=[5.204170427930421e-16],
                             change_mass=5.204170427930421e-16,
                             change_entropy=-0.00025256281830349225,
@@ -203,7 +203,7 @@ end
         @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                      "linear_advection_per_element.jl"),
                             solver=solver,
-                            l2=[0.0033676357851732484], linf=[0.009084487338805292],
+                            l2=[0.0023812781002627535], linf=[0.009084487338803626],
                             cons_error=[3.95516952522712e-16],
                             change_mass=-3.95516952522712e-16,
                             change_entropy=-0.00025256281830293714,
@@ -218,7 +218,7 @@ end
         @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                      "linear_advection_per_element.jl"),
                             solver=solver,
-                            l2=[0.0033676357851732484], linf=[0.009084487338805292],
+                            l2=[0.002381278100262957], linf=[0.009084487338803737],
                             cons_error=[8.604228440844963e-16],
                             change_mass=8.604228440844963e-16,
                             change_entropy=-0.0002525628183037143,
@@ -231,7 +231,7 @@ end
         @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                      "linear_advection_per_element.jl"),
                             Ds=Ds,
-                            l2=[0.0001574641423857773], linf=[0.00042895011704657815],
+                            l2=[0.00011134396287483692], linf=[0.00042895011704513486],
                             cons_error=[6.453171330633722e-16],
                             change_mass=-6.453171330633722e-16,
                             change_entropy=-9.082223085821184e-7,
@@ -246,7 +246,7 @@ end
         @test_trixi_include(joinpath(EXAMPLES_DIR_ADVECTION,
                                      "linear_advection_per_element.jl"),
                             Ds=Ds, mesh=mesh,
-                            l2=[0.0001574641423857773], linf=[0.00042895011704657815],
+                            l2=[0.0001113439628746496], linf=[0.00042895011704702224],
                             cons_error=[2.706168622523819e-16],
                             change_mass=-2.706168622523819e-16,
                             change_entropy=-9.082223089706964e-7,
